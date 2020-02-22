4377446e3e8238989f36d40271ab979557e24f2a
lpjune <laurenporsini@gmail.com>
2020-02-21 21:33:07 -0600

    14

---
+++ src/pages/login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class login extends Component {
+    render() {
+        return (
+            <div>
+                Login
+            </div>
+        )
+    }
+}
+
+export default login
